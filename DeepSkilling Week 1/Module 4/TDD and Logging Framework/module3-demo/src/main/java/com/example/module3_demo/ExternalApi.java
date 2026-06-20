235729fbfea13e25ec018d7b98e5c21e74fdacd6
Singanavena Sahasra <sahasrasweety4@gmail.com>
2026-06-20 17:46:06 +0530

    Delete DeepSkilling Week 1 directory

--- DeepSkilling Week 1/Module 4/TDD and Logging Framework/module3-demo/src/main/java/com/example/module3_demo/ExternalApi.java
+++
@@ -1,7 +0,0 @@
-package com.example.module3_demo;
-
-public interface ExternalApi {
-    String getData();                   
-    String getUserData(String userId);   
-    void logSession(String username);    
-}
